d4490a74377de5ea6ce3d443f73113e18ab0a5c1
hzhangal <hzhangal0330@gmail.com>
2017-11-27 21:24:05 +0800

    first test

--- parsing_document.py
+++ parsing_document.py
@@ -5,10 +5,13 @@ test_sentences = ['I love you', "It's so difficult to use Sling."]
 result = list()
 for test_sentence in test_sentences:
     command = "bazel-bin/nlp/parser/tools/parse --logtostderr --parser=sempar.flow --text=" + test_sentence + " --indent=2"
-    print('current command', command)
+    print('current command:', command)
     tmp_out = subprocess.check_output([command], shell=True)
     # tmp_output = tmp_out.stdout.decode('utf-8')
     # print('current output', tmp_output)
+    print('tmp_out:', tmp_out)
+    tmp_output = tmp_out.decode('utf-8')
+    print('tmp_output:', tmp_output)
     current_result = json.loads(tmp_out)
     result.append(current_result)
 
